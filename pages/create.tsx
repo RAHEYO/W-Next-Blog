1c09ac2483430cbaf899f4b21e8986569159fd49
Ryan Wang <ryanwhaoyu@gmail.com>
2023-06-10 20:59:30 -0700

    smt was wrong with git, pushing again

---
+++ pages/create.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next';
+
+import GeneralLayout from '@/components/GeneralLayout';
+
+type createProps = {
+
+}
+
+const CreatePage: NextPage<createProps> = () => {
+    return (
+    <GeneralLayout>
+
+    </GeneralLayout>
+    );
+}
+
+export default CreatePage;
